0558f09162814ce5a60ca3557331d53f49287472
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2025-05-12 10:18:25 -0300

    chore: reducing countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,10 +3,11 @@ import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, S
 import { useForm } from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
-    task: zod   
+    task: zod
         .string()
         .min(1, "Informe a tarefa"),
     minutesAmount: zod
@@ -20,7 +21,8 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 interface Cycle {
     id: string;
     task: string,
-    minutesAmount: number
+    minutesAmount: number,
+    startDate: Date,
 }
 
 export function Home() {
@@ -29,12 +31,22 @@ export function Home() {
     const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
     const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-        resolver: zodResolver(newCycleFormValidationSchema), 
+        resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
             minutesAmount: 0
         }
     });
+
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
+
+    useEffect(() => {
+        if (activeCycle) {
+            setInterval(() => {
+                setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+            }, 1000)
+        }
+    }, [activeCycle])
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         const id = String(new Date().getTime());
@@ -42,22 +54,21 @@ export function Home() {
         const newCycle: Cycle = {
             id: id,
             task: data.task,
-            minutesAmount: data.minutesAmount
+            minutesAmount: data.minutesAmount,
+            startDate: new Date(),
         }
 
         setCycles(state => [...state, newCycle])
         setActiveCycleId(id)
 
         reset();
-    }   
-
-    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
+    }
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
     const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
-    const minutesAmount = Math.floor(currentSeconds / 60 );
-    const secondsAmount = currentSeconds & 60;
+    const minutesAmount = Math.floor(currentSeconds / 60);
+    const secondsAmount = currentSeconds % 60;
 
     const minutes = String(minutesAmount).padStart(2, '0');
     const seconds = String(secondsAmount).padStart(2, '0');
@@ -91,7 +102,7 @@ export function Home() {
                         step={5}
                         min={5}
                         max={60}
-                        {...register('minutesAmount', {valueAsNumber: true})}
+                        {...register('minutesAmount', { valueAsNumber: true })}
                     />
 
                     <span>minutos.</span>
