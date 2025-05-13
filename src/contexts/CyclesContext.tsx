033cfaf6254e6b399d762ef37d7d2f52529c8353
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2025-05-13 14:49:31 -0300

    feat: mark cycle as finished on reducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -39,36 +39,52 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     const [cyclesState, dispatch] = useReducer(
         (state: CyclesState, action: any) => {
 
-            if (action.type === 'ADD_NEW_CYCLE') {
-                return {
-                    ...state,
-                    cycles: [...state.cycles, action.payload.newCycle],
-                    activeCycleId: action.payload.newCycle.id
-                }
-            }
+            switch (action.type) {
+                case 'ADD_NEW_CYCLE':
+                    return {
+                        ...state,
+                        cycles: [...state.cycles, action.payload.newCycle],
+                        activeCycleId: action.payload.newCycle.id
+                    }
 
-            if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
-                return {
-                    ...state,
-                    cycles: state.cycles.map((cycle) => {
-                        if (cycle.id === state.activeCycleId) {
-                            return { ...cycle, interruptedDate: new Date() }
-                        } else {
-                            return cycle;
-                        }
-                    }),
-                    activeCycleId: null,
-                }
-            }
+                case 'INTERRUPT_CURRENT_CYCLE':
+                    return {
+                        ...state,
+                        cycles: state.cycles.map((cycle) => {
+                            if (cycle.id === state.activeCycleId) {
+                                return { ...cycle, interruptedDate: new Date() }
+                            } else {
+                                return cycle;
+                            }
+                        }),
+                        activeCycleId: null,
+                    }
 
-            return state;
+                case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+                    return {
+                        ...state,
+                        cycles: state.cycles.map(
+                            (cycle) => {
+                                if (cycle.id === state.activeCycleId) {
+                                    return { ...cycle, finishedDate: new Date() }
+                                } else {
+                                    return cycle
+                                }
+                            }
+                        ),
+                        activeCycleId: null
+                    }
+
+                default: 
+                    return state;
+            }
         }, {
         cycles: [],
         activeCycleId: null,
     })
 
     const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-    const {cycles, activeCycleId} = cyclesState;
+    const { cycles, activeCycleId } = cyclesState;
 
     const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
 
@@ -83,15 +99,6 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
                 activeCycleId,
             }
         });
-        // setCycles((state) =>
-        //     state.map((cycle) => {
-        //         if (cycle.id === activeCycleId) {
-        //             return { ...cycle, finishedDate: new Date() }
-        //         } else {
-        //             return cycle
-        //         }
-        //     })
-        // )
     }
 
     function createNewCycle(data: CreateCycleData) {
@@ -103,8 +110,6 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
             minutesAmount: data.minutesAmount,
             startDate: new Date(),
         }
-
-        //setCycles(state => [...state, newCycle])
 
         dispatch({
             type: 'ADD_NEW_CYCLE',
